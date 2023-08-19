6f09bc0487c58b8db6a197f2279b90770512ee9a
Pavan Kusnuri <pavantejakusunuri@gmail.com>
2023-08-19 10:20:54 +0530

    Updated packages and Script in project

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom"
+
+
 const heading = React.createElement("div", {id: "parent"}, 
 [
-    React.createElement("div", {id: "child"}, "This is child"),
-    React.createElement("div", {id: "child"}, "This is child 2")
+    React.createElement("div", {id: "child"}, "This is First child"),
+    React.createElement("div", {id: "child"}, "This is Second child 2")
 ])
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(heading);
