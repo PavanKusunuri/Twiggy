03bbf52065decf694a81c3f65ccbfb3b555e8b17
Pavan Kusunuri <pavantejakusunuri@gmail.com>
2023-09-24 00:03:52 +0530

    New Changes much needed

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
+import Shimmer from './Shimmer';
 
 const Body = () => {
 
-    const [res, setRes] = useState(resList);
+    const [res, setRes] = useState([]);
+
+    useEffect(() => {
+        fetchData()
+    }, []);
+
+    const fetchData = async() => {
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.0005383&lng=81.8040345&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+        const json = await data.json();
+        setRes(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    }
+
+    if(res.length === 0){
+        return <Shimmer />
+    }
 
     return (
         <div className="body">
